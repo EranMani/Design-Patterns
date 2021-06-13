6ee21e9a99a28677ecb142b1f9d3c71bab1684df
EranMani <eranmanim@gmail.com>
2021-06-13 13:51:14 -0700

    Created game environment singleton to store NPC checkpoints Created pattern class Added idle and patrol states

---
+++ Assets/GameEnvironment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class GameEnvironment
+{
+    private static GameEnvironment instance;
+    private List<GameObject> checkPoints = new List<GameObject>();
+    public List<GameObject> CheckPoints { get { return CheckPoints; } }
+
+    public static GameEnvironment Singleton
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameEnvironment();
+                instance.CheckPoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+            }
+            return instance;
+        }
+    }
+   
+}
